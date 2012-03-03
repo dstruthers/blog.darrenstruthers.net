a7733548f6f58e4556f28d3c441e7376ee2e4acb
Darren Struthers <dstruthers@gmail.com>
2012-03-03 00:13:00 +0000

    Corrections to model definitions

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    """
+    Represents one post on the blog.
+    """
+    title = models.TextField(blank=False)
+    slug = models.SlugField(blank=False)
+    content = models.TextField(blank=False)
+    draft = models.BooleanField(default=True)
+    author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField()
+
+class Comment(models.Model):
+    """
+    Represents a comment on an Article
+    """
+    title = models.TextField()
+    content = models.TextField(blank=False)
+    article = models.ForeignKey(Article)
+    author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
